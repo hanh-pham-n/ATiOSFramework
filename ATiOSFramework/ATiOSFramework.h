d7bbc5d51b22cedab458c9e6c8dedbb1b45e96d1
hanh-pham-n <hanhphamn@gmail.com>
2017-11-21 15:53:26 +0700

    init

---
+++ ATiOSFramework/ATiOSFramework.h
@@ -0,0 +1,19 @@
+//
+//  ATiOSFramework.h
+//  ATiOSFramework
+//
+//  Created by Pham Ngoc Hanh on 11/21/17.
+//  Copyright © 2017 Pham Ngoc Hanh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ATiOSFramework.
+FOUNDATION_EXPORT double ATiOSFrameworkVersionNumber;
+
+//! Project version string for ATiOSFramework.
+FOUNDATION_EXPORT const unsigned char ATiOSFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ATiOSFramework/PublicHeader.h>
+
+
---
+++ ATiOSFramework/ATiOSFramework.swift
@@ -0,0 +1,15 @@
+//
+//  ATiOSFramework.swift
+//  ATiOSFramework
+//
+//  Created by Pham Ngoc Hanh on 11/21/17.
+//  Copyright © 2017 Pham Ngoc Hanh. All rights reserved.
+//
+
+import Foundation
+
+public class Hello {
+    class func hello() -> String {
+        return "hello"
+    }
+}
